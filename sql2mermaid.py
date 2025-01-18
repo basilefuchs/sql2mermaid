0efcf00fdb63110782314322e913e4ac1174dded
Basile Fuchs <157700598+basilefuchs@users.noreply.github.com>
2025-01-18 18:01:44 +0100

    Update sql2mermaid.py

--- sql2mermaid.py
+++ sql2mermaid.py
@@ -3,7 +3,18 @@ import re
 
 # add query here
 q = """
-# paste your query here
+SELECT 
+  a.patient_id,
+  a.last_name,
+  a.first_name,
+  b.drug,
+  b.date
+FROM employee a
+INNER JOIN drugs b on a.patient_id = b.patient_id
+LEFT JOIN deaths c on a.patient_id = c.patient_id
+WHERE
+c.patient_id is null
+ORDER BY b.date
 """
 
 def sql2mermaid(q) :

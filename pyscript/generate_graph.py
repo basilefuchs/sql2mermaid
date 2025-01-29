b8a823b0a6363a82c9285111cf9e26c50866cb1c
Arthur LAMARD <arthur.lamard@chu-brest.fr>
2025-01-29 09:08:32 +0100

    starting on front webpage

---
+++ pyscript/generate_graph.py
@@ -0,0 +1,10 @@
+from pyscript import document
+import sql2mermaid
+
+
+def generate_graph(event):
+    input_text = document.querySelector("#mermaid-code")
+    print("coucou")
+    sql_query = input_text.value
+    output_div = document.querySelector("#output")
+    output_div.innerText = sql2mermaid(sql_query)
